1f12c6aa16d874bd504ae406fdd346800d2704b7
Matthew Giannelli <matthew.giannelli@daugherty.com>
2018-10-20 21:02:59 -0400

    Revert "Created Goal entity"
    This reverts commit 79fccb56c2e2a938d364f8c6731100e04c008896.

--- src/main/java/com/mgnle/studentdatamanager/domain/enumeration/GoalTrackingMethod.java
+++
@@ -1,8 +0,0 @@
-package com.mgnle.studentdatamanager.domain.enumeration;
-
-/**
- * The GoalTrackingMethod enumeration.
- */
-public enum GoalTrackingMethod {
-    DATA_COLLECTION, WORK_SAMPLES
-}
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,14 +1,12 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { StudentDataManagerStudentModule } from './student/student.module';
-import { StudentDataManagerGoalModule } from './goal/goal.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
         StudentDataManagerStudentModule,
-        StudentDataManagerGoalModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
